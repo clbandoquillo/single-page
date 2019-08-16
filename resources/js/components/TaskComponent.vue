2eddb1d9b02fcefb19c3dc6a8bcb6e362fbe91af
clbandoquillo <bandoquillo.c33@gmail.com>
2019-08-16 21:15:12 +0800

    laravel codes

---
+++ resources/js/components/TaskComponent.vue
@@ -0,0 +1,54 @@
+<template>
+    <div>
+        <button class="btn btn-primary btn-block">Add New Task</button>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">id</th>
+                    <th scope="col">Name</th>
+                    <th scope="col">Body</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>1</td>
+                    <td>My Task</td>
+                    <td>Description of Task</td>
+                    <td><button class="btn btn-info">Edit</button></td>
+                    <td><button class="btn btn-danger">Delete</button></td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</template>
+
+<script>
+    export default {
+
+        data(){
+            return{
+                task:{
+                    name: '',
+                    body: ''
+                },
+
+                tasks: [],
+                uri: 'http://127.0.0.1:8000/'
+            }
+        },
+
+        methods: {
+
+            loadTask(){
+
+                axios.get(this.uri).then(response => {
+
+                });
+            }
+        },
+
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
